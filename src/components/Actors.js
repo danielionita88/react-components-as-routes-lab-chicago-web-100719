004b5ee98008054b552886f615e4d630f17ba42a
Daniel Ionita <danielionita88@yahoo.com>
2019-12-22 12:32:28 -0600

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { actors } from '../data';
 
+
+const renderActors=()=>{
+  return actors.map((actor,index)=><div key={index}>
+  <h3>Name: {actor.name}</h3>
+  <ul>
+    {actor.movies.map((movie,index)=> <li key={index}>
+    {movie}
+    </li>)}
+  </ul>
+
+  </div>)
+}
+
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { directors } from '../data';
 
+
+const renderDirectors=()=>{
+return directors.map((director,index)=><div key={index}>
+  <h3>Name: {director.name}</h3>
+  <p>Movies:</p>
+  <ul>
+    {director.movies.map((movie,index)=> <li key={index}>
+      {movie}
+    </li>)}
+  </ul>
+</div>)
+}
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {renderDirectors()}
     </div>
   );
 }
